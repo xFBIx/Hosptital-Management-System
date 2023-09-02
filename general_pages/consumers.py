70b007c7cb544f4a44950a7d44f06f7d587a4bd6
Rudradatt Dave <rudradattdave@gmail.com>
2023-09-02 12:18:22 +0530

    updates

--- general_pages/consumers.py
+++ general_pages/consumers.py
@@ -1,70 +1,76 @@
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 from django.contrib.auth.models import Group, User
+from asgiref.sync import sync_to_async
+
 
 class ChatRoomConsumer(AsyncWebsocketConsumer):
-    
     async def connect(self):
-        self.room_name = self.scope['url_route']['kwargs']['chat_box_name']
-        self.room_group_name = 'chat_%s' % self.room_name
-        
-        # username = str(self.scope["user"])
-        # user = User.objects.filter(username = username).first()
-        # if str(Group.obejcts.filter(user = user).first()) == "Patient":
-        #     self.patient_is_connected = True
-        # elif str(Group.obejcts.filter(user = user).first()) == "Staff":
-        #     self.staff_is_connected = True
-        # print(self.patient_is_connected)
-        # print(self.staff_is_connected)
+        self.room_name = self.scope["url_route"]["kwargs"]["chat_box_name"]
+        self.room_group_name = "chat_%s" % self.room_name
 
-        await self.channel_layer.group_add(
-            self.room_group_name,
-            self.channel_name
-        )
+        # self.patient_is_connected = False
+        # self.staff_is_connected = False
+        # username = str(self.scope["user"])
+        # async for user in User.objects.filter(username = username):
+        #     id = user.id
+        # async for grp in Group.objects.filter(user = id):
+        #     group = str(grp)
+        # if group == "Patient":
+        #     self.patient_is_connected = True
+        # elif group == "Staff":
+        #     self.staff_is_connected = True
+        # try:
+        #     print('patient', self.patient_is_connected)
+        # except:
+        #     pass
+        # try:
+        #     print('staff', self.staff_is_connected)
+        # except:
+        #     pass
+
+        await self.channel_layer.group_add(self.room_group_name, self.channel_name)
 
         await self.accept()
 
     async def disconnect(self, close_code):
-        
         # username = str(self.scope["user"])
-        # user = User.objects.filter(username = username).first()
-        # if str(Group.obejcts.filter(user = user).first()) == "Patient":
+        # async for user in User.objects.filter(username = username):
+        #     id = user.id
+        # async for grp in Group.objects.filter(user = id):
+        #     group = str(grp)
+        # if group == "Patient":
         #     self.patient_is_connected = False
-        # elif str(Group.obejcts.filter(user = user).first()) == "Staff":
+        # elif group == "Staff":
         #     self.staff_is_connected = False
-        # print(self.patient_is_connected)
-        # print(self.staff_is_connected)
-        
-        await self.channel_layer.group_discard(
-            self.room_group_name,
-            self.channel_name
-        )
+
+        await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
 
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
-        message = text_data_json['message']
-        username = text_data_json['username']
-        group = text_data_json['group']
+        message = text_data_json["message"]
+        username = text_data_json["username"]
+        group = text_data_json["group"]
 
         await self.channel_layer.group_send(
             self.room_group_name,
             {
-                'type': 'chatroom_message',
-                'message': message,
-                'username': username,
-                'group': group
-            }
+                "type": "chatroom_message",
+                "message": message,
+                "username": username,
+                "group": group,
+            },
         )
 
     async def chatroom_message(self, event):
-        message = event['message']
-        username = event['username']
-        group = event['group']
+        message = event["message"]
+        username = event["username"]
+        group = event["group"]
 
-        await self.send(text_data=json.dumps({
-            'message': message,
-            'username': username,
-            'group': group
-        }))
+        await self.send(
+            text_data=json.dumps(
+                {"message": message, "username": username, "group": group}
+            )
+        )
 
     pass
